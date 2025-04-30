4168c05c12b7b60443e752a5408862ffaecd295e
sogawa-yk <daybreaks.link@gmail.com>
2025-04-30 13:10:46 +0900

    fix

--- terraform/tenancy/user.tf
+++ terraform/tenancy/user.tf
@@ -2,14 +2,34 @@ locals {
   members = jsondecode(base64decode(var.members_file))
 }
 
-resource "oci_identity_user" "users" {
-  for_each = {
-    for team in local.members.teams, member in team.members :
+locals {
+  members = jsondecode(base64decode(var.members_file))
+
+  flattened_members = flatten([
+    for team in local.members.teams : [
+      for member in team.members : {
+        team_name  = team.name
+        username   = member.username
+        full_name  = member.full_name
+        email      = member.email
+        role       = member.role
+      }
+    ]
+  ])
+
+  member_map = {
+    for member in local.flattened_members :
     member.username => {
-      email       = member.email
-      description = "User ${member.full_name} from ${team.name} (${member.role})"
+      team_name  = member.team_name
+      full_name  = member.full_name
+      email      = member.email
+      role       = member.role
     }
   }
+}
+
+resource "oci_identity_user" "users" {
+  for_each = local.member_map
 
   name           = each.key
   email          = each.value.email
