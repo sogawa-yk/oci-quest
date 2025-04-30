3224fb9ca3cc8b5fc6f0f5e4863cde444b26acc0
sogawa-yk <daybreaks.link@gmail.com>
2025-04-30 13:14:30 +0900

    fix

--- terraform/tenancy/variables.tf
+++ terraform/tenancy/variables.tf
@@ -10,21 +10,6 @@ variable "members_file" {
   description = "Base64 encoded JSON string"
 }
 
-
-variable "teams" {
-  description = "List of teams and their members"
-  type = list(object({
-    name        = string
-    description = string
-    members     = list(object({
-      username   = string
-      full_name  = string
-      email      = string
-      role       = string
-    }))
-  }))
-}
-
 variable "compartment_ocid" {
 }
 
