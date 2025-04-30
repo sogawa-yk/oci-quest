7734c29924cd08e6b4906f03b79943602ce00459
sogawa-yk <daybreaks.link@gmail.com>
2025-04-30 14:51:34 +0900

    fix

--- terraform/tenancy/identity.tf
+++ terraform/tenancy/identity.tf
@@ -57,3 +57,15 @@ resource "oci_identity_user_group_membership" "test_user_group_membership" {
   user_id = oci_identity_user.users[each.key].id
   group_id = oci_identity_group.teams[each.value.team_name].id
 }
+
+resource "oci_identity_policy" "team_access" {
+  for_each = local.team_map
+
+  name           = "policy_${each.key}" 
+  description    = "Allow ${each.key} group to manage all-resources in compartment ${each.key}"
+  compartment_id = var.tenancy_ocid  
+
+  statements = [
+    "Allow group ${each.key} to manage all-resources in compartment ${each.key}"
+  ]
+}
